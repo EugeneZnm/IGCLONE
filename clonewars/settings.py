e38db5ff1813b1ef9fb86f19d410dc5d83f6b0c2
EugeneZnm <eugenenzioki@gmail.com>
2018-09-30 20:54:01 +0300

    connecting postgres to application

--- clonewars/settings.py
+++ clonewars/settings.py
@@ -77,8 +77,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'igclone',
-        'USER': 'eugene'.
-    'PASSWORD': 'necromancer'
+        'USER': 'eugene',
+        'PASSWORD': 'necromancer'
     }
 }
 
@@ -107,7 +107,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Africa/Nairobi'
 
 USE_I18N = True
 

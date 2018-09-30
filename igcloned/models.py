0a451ff7a2bef342ab0c9c8a37392d989f2cd80c
EugeneZnm <eugenenzioki@gmail.com>
2018-09-30 22:39:11 +0300

    creation of image model

--- igcloned/models.py
+++ igcloned/models.py
@@ -1,3 +1,16 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+
+class Image(models.Models):
+    """
+    Image model creating table
+    """
+    image = models.ImageField(upload_to='media/', blank=True)
+    image_name = models.CharField(max_length=200)
+    caption = models.CharField(max_length=1000)
+    likes = models.IntegerField(default=0)
+    comment = models.CharField(max_length=10000)
+
+

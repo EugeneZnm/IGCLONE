510de961c2256a25016c45860c587abe87d58d15
EugeneZnm <eugenenzioki@gmail.com>
2018-10-04 11:02:19 +0300

    addition of signup form in view function and forms

--- igcloned/views.py
+++ igcloned/views.py
@@ -1,5 +1,17 @@
 from django.shortcuts import render
 
+# import django UserCreation Form
+from django.contrib.auth.forms import  UserCreationForm
+
+
 # Create your views here.
+
+
+# SIGNUP VIEW FUNCTION
 def signup(request):
-    return render(request, 'registration/registration_form.html')
+    """
+    signup form view function
+    """
+    form = UserCreationForm()
+    return render(request, 'registration/registration_form.html', {'form':form})
+

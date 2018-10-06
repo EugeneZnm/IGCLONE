86941784a7df682f6aea0678806b1141500c2068
EugeneZnm <eugenenzioki@gmail.com>
2018-10-06 15:55:40 +0300

    image upload forms

--- igcloned/forms.py
+++ igcloned/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-from .models import Profile
+from .models import Profile, Image
 
 
 class SignUpForm(UserCreationForm):
@@ -25,5 +25,5 @@ class EditProfileForm(forms.ModelForm):
 
 class UploadImageForm(forms.ModelForm):
     class Meta:
-        model = image
+        model = Image
         fields = ['image', 'image_name', 'caption']
